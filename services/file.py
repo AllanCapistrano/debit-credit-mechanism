a808341008fa5e37a6ff65e5caadf6096edb4875
AllanCapistrano <allan.capistrano3@gmail.com>
2021-09-16 16:26:16 -0300

    Add writeTransations function.

--- services/file.py
+++ services/file.py
@@ -17,3 +17,17 @@ class File:
         f = open(fileName, "w")
 
         f.close()
+
+    def writeTransations(self, fileName: str, transation: str) -> None:
+        """ Write the transation in the file.
+
+        Parameters
+        -----------
+        fileName: :class:`str`
+            File name.
+        transation: :class:`str`
+            Transation... 
+        """
+        
+        with open(fileName, "a") as f:
+            f.write(transation)
